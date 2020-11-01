e556e581751dc70aaa424d19d3e426d4318c7df3
shinjawkwang <shinjawkwang@gmail.com>
2020-11-01 14:05:34 +0000

    :art: ordered objects and imports by alphabet

--- src/utils/userMenuButtonContent.js
+++ src/utils/userMenuButtonContent.js
@@ -1,8 +1,8 @@
 import { 
-	faQuestion,
+	faDoorOpen,
 	faKey,
+	faQuestion,
 	faTools,
-	faDoorOpen 
 } from '@fortawesome/free-solid-svg-icons';
 import {
 	faCalendarAlt,
@@ -10,15 +10,15 @@ import {
 	faPaperPlane
 } from '@fortawesome/free-regular-svg-icons';
 
-const switchCalendar = { handler: 'switchCalendar', label: '시간표 전환', icon: faCalendarAlt };
 const addEvent = { handler: 'openAddEventModal', label: '새 일정 추가', icon: faCalendarPlus };
-const borrowRequest = { handler: 'openBorrowRequestModal', label: '대여 요청', icon: faPaperPlane };
 const adminPage = { handler: 'adminPage', label: '관리자 페이지', icon: faTools };
-const setCalendar = { handler: 'openSetCalendarModal', label: '시간표 등록', icon: faCalendarPlus };
-const help = { handler: 'openHelpModal', label: '도움말', icon: faQuestion };
+const borrowRequest = { handler: 'openBorrowRequestModal', label: '대여 요청', icon: faPaperPlane };
 const changePassword = { handler: 'openChangePasswordModal', label: '비밀번호 변경', icon: faKey };
+const help = { handler: 'openHelpModal', label: '도움말', icon: faQuestion };
 const login = { handler: 'openLoginModal', label: '로그인', icon: faDoorOpen };
 const logout = { handler: 'handleLogout', label: '로그아웃', icon: faDoorOpen };
+const setCalendar = { handler: 'openSetCalendarModal', label: '시간표 등록', icon: faCalendarPlus };
+const switchCalendar = { handler: 'switchCalendar', label: '시간표 전환', icon: faCalendarAlt };
 
 const clubType = {
 	clubAdmin: [ switchCalendar, addEvent, borrowRequest, help, changePassword, logout ],
