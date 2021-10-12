d3359c634ce9c004222a5dcb4806aa328903f8d2
jaepang <jake_king@kakao.com>
2021-10-13 02:14:54 +0900

    :racehorse: added login to guest usermenu

--- src/utils/userMenuButtonContent.js
+++ src/utils/userMenuButtonContent.js
@@ -3,6 +3,7 @@ import {
 	faQuestion,
 	faShare,
 	faTools,
+	faSignInAlt
 } from '@fortawesome/free-solid-svg-icons';
 import {
 	faCalendarPlus,
@@ -13,6 +14,7 @@ import {
 const adminPage = { handler: 'adminPage', label: '관리자 페이지', icon: faTools };
 const borrowRequest = { handler: 'openBorrowRequestModal', label: '대여 요청', icon: faPaperPlane };
 const borrowSubmit = { handler: 'enableBorrowTimeMode', label: '대여 등록', icon: faShare };
+const login = { handler: 'openLoginModal', label: '로그인', icon: faSignInAlt}
 const changePassword = { handler: 'openChangePasswordModal', label: '비밀번호 변경', icon: faKey };
 const help = { handler: 'openHelpModal', label: '도움말', icon: faQuestion };
 const setCalendar = { handler: 'enableSetCalendarMode', label: '시간표 등록', icon: faCalendarPlus };
@@ -21,7 +23,7 @@ const clubType = {
 	clubAdmin: [ setCalendar, borrowSubmit, borrowRequest, help, changePassword ],
 	club: [ help, changePassword],
 	admin: [ adminPage, setCalendar, changePassword],
-	guest: [ borrowRequest, help]
+	guest: [ login, borrowRequest, help ]
 };
 
 export default clubType;
