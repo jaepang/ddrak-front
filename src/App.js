3b85558d0395720d5a099887d35f46049ef8e7d5
jaepang <jake_king@kakao.com>
2021-10-16 16:41:27 +0900

    global style & package-lock minor update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import MainStructure from './components/MainStructure';
-import './styles/App.css';
+import { Global, css } from '@emotion/core';
+//import './styles/App.css';
 
 class App extends Component {
     render() {
         return (
-			<MainStructure/>
+            <div>
+                <Global styles={globalStyle}/>
+			    <MainStructure/>
+            </div>
         );
     }
 }
+
+const globalStyle = css `
+    * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+
+    body {
+        margin: 0 !important;
+        padding: 0 !important;
+        overflow: hidden;
+        background-color: #FFF;
+    }
+`;
 
 export default App;
 
